63d96bfa03c9299b8257625ceb5af1e54f4f2b1c
Vijayalakshmi S <Vijayalakshmi S@DESKTOP-QNBP0FC>
2023-01-04 15:55:02 +0530

    Project

---
+++ src/test/java/scripts/Tc2.java
@@ -0,0 +1,15 @@
+package scripts;
+
+	import org.testng.Reporter;
+	import org.testng.annotations.Test;
+
+	public class Tc2 {
+		@Test
+		public void demo() {
+			Reporter.log("hello Selenium",true);
+		}
+		
+
+	}
+
+
---
+++ src/test/java/scripts/Tc4.java
@@ -0,0 +1,16 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc4 {
+		@Test
+		public void demo() {
+			Reporter.log("hello PythonSelenium",true);
+		}
+		
+
+	}
+
+
+
